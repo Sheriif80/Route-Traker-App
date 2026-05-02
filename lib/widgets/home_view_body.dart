9fede7580844e746e9007c6f919f2eaecfccc42a
Sherif80 <sherifashraf1054@gmail.com>
2026-05-03 00:44:34 +0300

    Created a method to fetch auto complete places

--- lib/widgets/home_view_body.dart
+++ lib/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:route_tracker_app/services/google_maps_places_service.dart';
 import 'package:route_tracker_app/widgets/custom_google_maps.dart';
 import 'package:route_tracker_app/widgets/custom_text_field.dart';
 
@@ -11,11 +12,22 @@ class HomeViewBody extends StatefulWidget {
 
 class _HomeViewBodyState extends State<HomeViewBody> {
   late TextEditingController _textController;
+  late GoogleMapsPlacesService googleMapsPlacesService;
 
   @override
   void initState() {
     super.initState();
     _textController = TextEditingController();
+    googleMapsPlacesService = GoogleMapsPlacesServiceImpl();
+    fetchAutoCompletePlaces();
+  }
+
+  void fetchAutoCompletePlaces() {
+    _textController.addListener(() {
+      if (_textController.text.length >= 3) {
+        googleMapsPlacesService.getPlaceAutoComplete(_textController.text);
+      }
+    });
   }
 
   @override
