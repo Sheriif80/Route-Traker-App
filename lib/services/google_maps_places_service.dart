bb7e76e0361ffcdefcc55b12353da7330294f898
Sherif80 <sherifashraf1054@gmail.com>
2026-05-04 03:00:40 +0300

    Created getRoutes method and implemented it using locationIQ for now

--- lib/services/google_maps_places_service.dart
+++ lib/services/google_maps_places_service.dart
@@ -5,22 +5,36 @@ import 'package:dio/dio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:route_tracker_app/errors/failures.dart';
 import 'package:route_tracker_app/models/place_auto_complete_model/place_auto_complete_model.dart';
+import 'package:route_tracker_app/models/routes_model/routes_model.dart';
 
 abstract class GoogleMapsPlacesService {
+  // For sorry, I'll be working on LocationIQ service instead of Google Maps,
+  // as I don't have access to Google Maps.
+  // That's because of the billing methods needed to use Google Maps API key.
   Future<Either<Failures, List<PlaceAutoCompleteModel>>> getPlaceAutoComplete(
     String placeText,
   );
+  Future<Either<Failures, RoutesModel>> getRoutes({
+    required String startLongitude,
+    required String startLatitude,
+    required String endLongitude,
+    required String endLatitude,
+  });
 }
 
 class GoogleMapsPlacesServiceImpl implements GoogleMapsPlacesService {
+  // For sorry, I'll be working on LocationIQ service instead of Google Maps,
+  // as I don't have access to Google Maps.
+  // That's because of the billing methods needed to use Google Maps API key.
+
+  final String baseUrl = 'https://api.locationiq.com/';
   final String apiKey = dotenv.env['locationIQ_ApiKey']!;
+  final Dio dio = Dio();
   @override
   Future<Either<Failures, List<PlaceAutoCompleteModel>>> getPlaceAutoComplete(
     String placeText,
   ) async {
     try {
-      final Dio dio = Dio();
-      final String baseUrl = 'https://api.locationiq.com/';
       final String autoCompleteEndPoint = 'v1/autocomplete';
 
       final response = await dio.get(
@@ -33,6 +47,44 @@ class GoogleMapsPlacesServiceImpl implements GoogleMapsPlacesService {
             .map((e) => PlaceAutoCompleteModel.fromJson(e))
             .toList();
         return right(places);
+      } else {
+        return left(ServerFailure('Something went wrong'));
+      }
+    } on DioException catch (e) {
+      log(e.message ?? 'Something went wrong');
+      return left(ServerFailure(e.message ?? 'Something went wrong'));
+    } catch (e) {
+      log(e.toString());
+      return left(ServerFailure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failures, RoutesModel>> getRoutes({
+    required String startLongitude,
+    required String startLatitude,
+    required String endLongitude,
+    required String endLatitude,
+  }) async {
+    try {
+      // Actually, I'm using directions API not routes API
+      // bcs routes API not available in LocationIQ.
+
+      final String routesEndPoint = 'v1/directions/driving/';
+      final String start = "$startLongitude,$startLatitude";
+      final String end = "$endLongitude,$endLatitude";
+      final response = await dio.get(
+        '$baseUrl$routesEndPoint$start;$end',
+        queryParameters: {
+          'key': apiKey,
+
+          // 'alternatives': 'true',
+          // 'steps': 'true',
+        },
+      );
+      if (response.statusCode == 200 && response.data != null) {
+        final RoutesModel routes = RoutesModel.fromJson(response.data);
+        return right(routes);
       } else {
         return left(ServerFailure('Something went wrong'));
       }
